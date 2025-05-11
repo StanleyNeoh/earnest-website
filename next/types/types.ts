bb0a46731ba6e39685fbd80cacec643886a7616f
stanleyneoh <neohstanley@gmail.com>
2025-05-12 03:19:37 +0800

    Fixes type error

--- next/types/types.ts
+++ next/types/types.ts
@@ -1,5 +1,23 @@
 import { ParagraphStoryProps } from "./components/dynamic-zone";
 
+export interface Category {
+  name: string;
+}
+
+export interface Article {
+  title: string;
+  description: string;
+  slug: string;
+  content: string;
+  dynamic_zone: any[];
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  locale: string;
+  image: Image;
+  categories: Category[]
+}
+
 export interface Image {
   url: string;
   alternativeText: string;
