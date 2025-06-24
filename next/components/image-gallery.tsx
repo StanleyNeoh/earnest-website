eb7166c5c6ec82526daca37170661f115d99e465
stanleyneoh <neohstanley@gmail.com>
2025-06-25 02:07:38 +0800

    Adds nextjs to image gallery

--- next/components/image-gallery.tsx
+++ next/components/image-gallery.tsx
@@ -2,7 +2,7 @@
 import React, { useMemo } from "react";
 import { RowsPhotoAlbum } from "react-photo-album";
 import "react-photo-album/rows.css";
-import { Image } from "@/types/types";
+import { Image as ImageType } from "@/types/types";
 
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
@@ -13,13 +13,74 @@ import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import { strapiImage } from "@/lib/strapi/strapiImage";
+import {
+  isImageFitCover,
+  isImageSlide,
+  useLightboxProps,
+  useLightboxState,
+} from "yet-another-react-lightbox";
+import Image from "next/image";
+
+function isNextJsImage(slide: any) {
+  return (
+    isImageSlide(slide) &&
+    typeof slide.width === "number" &&
+    typeof slide.height === "number"
+  );
+}
+
+function NextJsImage({ slide, offset, rect }: any) {
+  const {
+    on: { click },
+    carousel: { imageFit },
+  } = useLightboxProps();
+
+  const { currentIndex } = useLightboxState();
+
+  const cover = isImageSlide(slide) && isImageFitCover(slide, imageFit);
+
+  if (!isNextJsImage(slide)) return undefined;
+
+  const width = !cover
+    ? Math.round(
+        Math.min(rect.width, (rect.height / slide.height) * slide.width),
+      )
+    : rect.width;
+
+  const height = !cover
+    ? Math.round(
+        Math.min(rect.height, (rect.width / slide.width) * slide.height),
+      )
+    : rect.height;
+
+  return (
+    <div style={{ position: "relative", width, height }}>
+      <Image
+        fill
+        alt=""
+        src={slide}
+        loading="eager"
+        draggable={false}
+        placeholder={slide.blurDataURL ? "blur" : undefined}
+        style={{
+          objectFit: cover ? "cover" : "contain",
+          cursor: click ? "pointer" : undefined,
+        }}
+        sizes={`${Math.ceil((width / window.innerWidth) * 100)}vw`}
+        onClick={
+          offset === 0 ? () => click?.({ index: currentIndex }) : undefined
+        }
+      />
+    </div>
+  );
+}
 
 export const ImageGallery = ({
   images,
   maxNumber,
   isStrapiImage = false,
 }: {
-  images?: Image[];
+  images?: ImageType[];
   maxNumber?: number;
   isStrapiImage?: boolean;
 }) => {
@@ -48,6 +109,7 @@ export const ImageGallery = ({
         index={index}
         close={() => setIndex(-1)}
         plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+        render={{ slide: NextJsImage }}
       />
     </>
   )
