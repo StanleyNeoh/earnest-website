523c1b29b69bc3089a0a1323ca57a661f8090ef4
stanleyneoh <neohstanley@gmail.com>
2025-06-26 02:31:34 +0800

    Adds layout export

--- next/app/[locale]/(top-margin)/services/layout.tsx
+++ next/app/[locale]/(top-margin)/services/layout.tsx
@@ -1,3 +1,10 @@
 import seoJson from '@/seo.json';
 export const metadata = seoJson.main.services;
 
+export default function ServicesPageLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return children
+}
